01d4903727beff89574ca4e434862bf1f40dc5e6
Aubrey Selamu-Bell <aubrey.selamu@Aubreys-MacBook-Pro.local>
2021-11-17 07:53:31 -0500

    implemented HTTP request with AJAX/axios and check word for validity

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from boggle import Boggle
-from flask import Flask, session, request, redirect, render_template
+from flask import Flask, session, request, redirect, render_template, jsonify
 from testing import add
 
 
@@ -9,8 +9,18 @@ app.config['SECRET_KEY'] = "abc123"
 boggle_game = Boggle()
 
 @app.route('/')
-def test():
+def homepage():
     board = boggle_game.make_board()
     session['board'] = board
-    return render_template('base.html', board = board)
+    return render_template('index.html', board = board)
 
+@app.route('/check-word')
+def check_word():
+    '''Check if word is in dictionary'''
+    
+    word = request.args['word']
+    board = session['board']
+    
+    response = boggle_game.check_valid_word(board, word)
+
+    return jsonify({'result' : response})
